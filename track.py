13a294db42f8b3be44b7dcb71850925b17116757
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2019-01-10 20:35:44 +0200

    added track script

---
+++ track.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://www.elta.gr/en-us/personal/tracktrace.aspx?qc="
+
+print("Enter your tracking number: ")
+tracking_number = input()
+
+url = url + tracking_number 
+
+page = requests.get(url)
+
+soup = BeautifulSoup(page.text, "html.parser")
+
+rows = []
+
+table = soup.find("table", {"class": "tg"})
+
+for row in table.find_all("tr"):
+    rows.append(row.text)
+
+for r in reversed(rows[2: -1]):
+    print(r)
